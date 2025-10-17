b2ce0e55241111a12a708b3691aa88042362f29d
Léo Prosper <leoprosper10@gmail.com>
2025-10-17 11:21:02 +0200

    Update database insertion script for Porsche project

--- 3_insertion.sql
+++ 3_insertion.sql
@@ -4,7 +4,7 @@
 -- Cohérent avec 1_creation.sql et 2_contraintes.sql
 -- ======================================================
 
-USE porsche_db;
+USE porsche_concession_db;
 
 -- ========================
 -- 1) CLIENT (10)
@@ -23,7 +23,6 @@ INSERT INTO Client (client_ID, client_num_permis, client_nom, client_prenom) VAL
 
 -- ========================
 -- 2) COORDONNEES (10)
--- (téléphones 06/07 -> INT entre 600000000 et 799999999)
 -- ========================
 INSERT INTO Coordonnees (client_ID, telephone_portable, mail, telephone_fixe) VALUES
 (1, 678123456, 'pierre.dupont1@exemple.fr', 142345678),
@@ -61,7 +60,6 @@ INSERT INTO Habiter (client_ID, adresse_ID) VALUES
 
 -- ========================
 -- 5) STATUT_COMMANDE (10)
--- (valeurs autorisées: 'En attente','Validée','En production','Livrée','Annulée')
 -- ========================
 INSERT INTO Statut_commande (statut_com_ID, statut_com_libelle, statut_com_date, statut_com_commentaire) VALUES
 (1, 'En attente',     '2025-01-05', 'Commande enregistrée'),
@@ -77,7 +75,6 @@ INSERT INTO Statut_commande (statut_com_ID, statut_com_libelle, statut_com_date,
 
 -- ========================
 -- 6) FACTURE (10)
--- (TVA >=0, TTC >= HT)
 -- ========================
 INSERT INTO Facture (facture_ID, facture_type, facture_date, montant_HT, TVA, montant_TTC) VALUES
 (1, 'Vente', '2025-03-16', 120000, 24000, 144000),
@@ -93,7 +90,6 @@ INSERT INTO Facture (facture_ID, facture_type, facture_date, montant_HT, TVA, mo
 
 -- ========================
 -- 7) CONFIGURATION (10)
--- (énergie: 'Essence','Diesel','Hybride','Electrique'; transmission: 'Manuelle','Automatique')
 -- ========================
 INSERT INTO Configuration (config_ID, config_modele, config_interieur, config_energie, config_transmission, config_code_couleur) VALUES
 (1, '911 Carrera',         'Cuir Noir',      'Essence',     'Automatique', 2130),
@@ -108,14 +104,14 @@ INSERT INTO Configuration (config_ID, config_modele, config_interieur, config_en
 (10,'Panamera 4 E-Hybrid', 'Cuir Noir',      'Hybride',     'Automatique', 12);
 
 -- ========================
--- 8) OPTION (2 seulement par conception: BOOLEAN 0/1)
+-- 8) OPTION VEHICULE (2)
 -- ========================
 INSERT INTO OptionVehicule (option_ID, option_libelle) VALUES
 (0, 'Pack Sport'),
 (1, 'Toit ouvrant');
 
 -- ========================
--- 9) CHOIX (20) - chaque config prend 2 options (0 et 1)
+-- 9) CHOIX (20)
 -- ========================
 INSERT INTO Choix (config_ID, option_ID) VALUES
 (1,0),(1,1),
@@ -131,91 +127,81 @@ INSERT INTO Choix (config_ID, option_ID) VALUES
 
 -- ========================
 -- 10) REPRISE (10)
--- (rep_estimation_prix >=0; rep_etat_vehicule parmi 'Excellent','Bon','Moyen','Mauvais')
 -- ========================
 INSERT INTO Reprise (rep_ID, rep_marque, rep_modele, rep_VIN, rep_immat, rep_estimation_prix, rep_etat_vehicule) VALUES
-(1, 'Audi',     'A6',        541236789, 'AA-123-AA', 22000, 'Bon'),
-(2, 'BMW',      '330i',      612349875, 'BB-234-BB', 18000, 'Moyen'),
-(3, 'Mercedes', 'C200',      723498516, 'CC-345-CC', 20000, 'Bon'),
-(4, 'Porsche',  'Macan',     734985162, 'DD-456-DD', 45000, 'Excellent'),
-(5, 'Renault',  'Megane',    812349756, 'EE-567-EE',  7000, 'Moyen'),
-(6, 'Peugeot',  '3008',      934875621, 'FF-678-FF', 14000, 'Bon'),
-(7, 'Volkswagen','Golf 7',   845612379, 'GG-789-GG',  9000, 'Moyen'),
-(8, 'Tesla',    'Model 3',   956123478, 'HH-890-HH', 28000, 'Bon'),
-(9, 'Toyota',   'RAV4',      967834125, 'II-901-II', 16000, 'Bon'),
-(10,'Alfa Romeo','Giulia',   978345612, 'JJ-012-JJ', 19000, 'Bon');
+(1, 'Audi', 'A6', 541236789, 'AA-123-AA', 22000, 'Bon'),
+(2, 'BMW', '330i', 612349875, 'BB-234-BB', 18000, 'Moyen'),
+(3, 'Mercedes', 'C200', 723498516, 'CC-345-CC', 20000, 'Bon'),
+(4, 'Porsche', 'Macan', 734985162, 'DD-456-DD', 45000, 'Excellent'),
+(5, 'Renault', 'Megane', 812349756, 'EE-567-EE', 7000, 'Moyen'),
+(6, 'Peugeot', '3008', 934875621, 'FF-678-FF', 14000, 'Bon'),
+(7, 'Volkswagen', 'Golf 7', 845612379, 'GG-789-GG', 9000, 'Moyen'),
+(8, 'Tesla', 'Model 3', 956123478, 'HH-890-HH', 28000, 'Bon'),
+(9, 'Toyota', 'RAV4', 967834125, 'II-901-II', 16000, 'Bon'),
+(10, 'Alfa Romeo', 'Giulia', 978345612, 'JJ-012-JJ', 19000, 'Bon');
 
 -- ========================
--- 11) COMMANDE (10)
--- (statut parmi: 'En attente','Confirmée','Annulée','Livrée'; mode_financement: 'Comptant','Crédit','Leasing')
--- NOTE: Notre MPD inclut client_ID (FK) -> on le renseigne ici.
--- ========================
-INSERT INTO Commande (commande_ID, commande_date, n_commission_usine, commande_statut, mode_financement, accompte, rep_ID, facture_ID, statut_com_ID, config_ID, client_ID) VALUES
-(1,  '2025-02-01', 9920001, 'Confirmée', 'Comptant', 1, 1, 1, 1, 1, 1),
-(2,  '2025-02-05', 9920002, 'En attente','Crédit',   0, 2, 2, 2, 2, 2),
-(3,  '2025-02-12', 9711003, 'Confirmée', 'Leasing',  1, 3, 3, 3, 3, 3),
-(4,  '2025-02-20', 9Y00004, 'Livrée',    'Comptant', 1, 4, 4, 4, 4, 4),
-(5,  '2025-03-01', 9920005, 'Confirmée', 'Crédit',   0, 5, 5, 5, 5, 5),
-(6,  '2025-03-10', 9711006, 'En attente','Leasing',  0, 6, 6, 6, 6, 6),
-(7,  '2025-03-18', 9Y00007, 'Annulée',   'Crédit',   0, 7, 7, 7, 7, 7),
-(8,  '2025-03-25', 9920008, 'Livrée',    'Comptant', 1, 8, 8, 8, 8, 8),
-(9,  '2025-04-02', 9711009, 'Confirmée', 'Leasing',  1, 9, 9, 9, 9, 9),
-(10, '2025-04-09', 9Y00010, 'En attente','Crédit',   0,10,10,10,10,10);
-
--- ========================
--- 12) VEHICULE (10)
--- (VIN = INT dans notre MPD; vehicule_statut parmi 'Disponible','Vendu','Réservé')
--- ========================
-INSERT INTO Vehicule (VIN, vehicule_annee, vehicule_code_pays, vehicule_kilometrage, vehicule_statut, garantie_ID, commande_ID) VALUES
-(100000001, '2025-01-01', 250, 0,    'Vendu',     1, 1),
-(100000002, '2025-01-01', 250, 0,    'Réservé',   2, 2),
-(100000003, '2025-01-01', 250, 15,   'Vendu',     3, 3),
-(100000004, '2025-01-01', 250, 10,   'Vendu',     4, 4),
-(100000005, '2025-01-01', 250, 0,    'Réservé',   5, 5),
-(100000006, '2025-01-01', 250, 0,    'Disponible',6, 6),
-(100000007, '2025-01-01', 250, 0,    'Annulé',    NULL, NULL), -- sera corrigé via statut cohérent si besoin
-(100000008, '2025-01-01', 250, 5,    'Vendu',     8, 8),
-(100000009, '2025-01-01', 250, 0,    'Réservé',   9, 9),
-(100000010,'2025-01-01', 250, 0,    'Disponible',10,10);
-
--- Correction: 'Annulé' n'est pas une valeur autorisée pour vehicule_statut -> on met 'Disponible'
-UPDATE Vehicule SET vehicule_statut='Disponible' WHERE VIN=100000007;
-
--- ========================
--- 13) LIVRAISON (10)
--- (livraison_checks_docs: 'Oui'/'Non')
--- ========================
-INSERT INTO Livraison (livraison_ID, livraison_date, livraison_lieu, livraison_checks_docs, commande_ID) VALUES
-(1, '2025-03-15', 'Centre Porsche Paris',     'Oui', 1),
-(2, '2025-04-12', 'Centre Porsche Lyon',      'Oui', 2),
-(3, '2025-04-22', 'Centre Porsche Bordeaux',  'Non', 3),
-(4, '2025-05-03', 'Centre Porsche Paris',     'Oui', 4),
-(5, '2025-05-19', 'Centre Porsche Lyon',      'Non', 5),
-(6, '2025-06-02', 'Centre Porsche Marseille', 'Oui', 6),
-(7, '2025-06-16', 'Centre Porsche Rennes',    'Oui', 7),
-(8, '2025-07-04', 'Centre Porsche Paris',     'Oui', 8),
-(9, '2025-07-23', 'Centre Porsche Bordeaux',  'Oui', 9),
-(10,'2025-08-06', 'Centre Porsche Lille',     'Non', 10);
-
--- ========================
--- 14) GARANTIE (10)
--- (date_fin > date_debut)
+-- 11) GARANTIE (10)
 -- ========================
 INSERT INTO Garantie (garantie_ID, garantie_type, garantie_date_debut, garantie_date_fin) VALUES
-(1,  'Porsche Approved', '2025-03-15', '2027-03-15'),
-(2,  'Neuf',             '2025-04-12', '2027-04-12'),
-(3,  'Neuf',             '2025-04-22', '2027-04-22'),
-(4,  'Porsche Approved', '2025-05-03', '2027-05-03'),
-(5,  'Neuf',             '2025-05-19', '2027-05-19'),
-(6,  'Neuf',             '2025-06-02', '2027-06-02'),
-(7,  'Porsche Approved', '2025-06-16', '2027-06-16'),
-(8,  'Neuf',             '2025-07-04', '2027-07-04'),
-(9,  'Porsche Approved', '2025-07-23', '2027-07-23'),
-(10, 'Neuf',             '2025-08-06', '2027-08-06');
+(1, 'Porsche Approved', '2025-03-15', '2027-03-15'),
+(2, 'Neuf', '2025-04-12', '2027-04-12'),
+(3, 'Neuf', '2025-04-22', '2027-04-22'),
+(4, 'Porsche Approved', '2025-05-03', '2027-05-03'),
+(5, 'Neuf', '2025-05-19', '2027-05-19'),
+(6, 'Neuf', '2025-06-02', '2027-06-02'),
+(7, 'Porsche Approved', '2025-06-16', '2027-06-16'),
+(8, 'Neuf', '2025-07-04', '2027-07-04'),
+(9, 'Porsche Approved', '2025-07-23', '2027-07-23'),
+(10, 'Neuf', '2025-08-06', '2027-08-06');
+
+-- ========================
+-- 12) COMMANDE (10)
+-- ========================
+INSERT INTO Commande (commande_ID, commande_date, n_commission_usine, commande_statut, mode_financement, accompte, rep_ID, facture_ID, statut_com_ID, config_ID, client_ID) VALUES
+(1, '2025-02-01', '9920001', 'Confirmée', 'Comptant', 1, 1, 1, 1, 1, 1),
+(2, '2025-02-05', '9920002', 'En attente', 'Crédit', 0, 2, 2, 2, 2, 2),
+(3, '2025-02-12', '9711003', 'Confirmée', 'Leasing', 1, 3, 3, 3, 3, 3),
+(4, '2025-02-20', '9Y00004', 'Livrée', 'Comptant', 1, 4, 4, 4, 4, 4),
+(5, '2025-03-01', '9920005', 'Confirmée', 'Crédit', 0, 5, 5, 5, 5, 5),
+(6, '2025-03-10', '9711006', 'En attente', 'Leasing', 0, 6, 6, 6, 6, 6),
+(7, '2025-03-18', '9Y00007', 'Annulée', 'Crédit', 0, 7, 7, 7, 7, 7),
+(8, '2025-03-25', '9920008', 'Livrée', 'Comptant', 1, 8, 8, 8, 8, 8),
+(9, '2025-04-02', '9711009', 'Confirmée', 'Leasing', 1, 9, 9, 9, 9, 9),
+(10,'2025-04-09', '9Y00010', 'En attente', 'Crédit', 0,10,10,10,10,10);
+
+-- ========================
+-- 13) VEHICULE (10)
+-- ========================
+INSERT INTO Vehicule (VIN, vehicule_annee, vehicule_code_pays, vehicule_kilometrage, vehicule_statut, garantie_ID, commande_ID) VALUES
+(100000001, '2025-01-01', 250, 0, 'Vendu', 1, 1),
+(100000002, '2025-01-01', 250, 0, 'Réservé', 2, 2),
+(100000003, '2025-01-01', 250, 15, 'Vendu', 3, 3),
+(100000004, '2025-01-01', 250, 10, 'Vendu', 4, 4),
+(100000005, '2025-01-01', 250, 0, 'Réservé', 5, 5),
+(100000006, '2025-01-01', 250, 0, 'Disponible', 6, 6),
+(100000007, '2025-01-01', 250, 0, 'Disponible', 7, 7),
+(100000008, '2025-01-01', 250, 5, 'Vendu', 8, 8),
+(100000009, '2025-01-01', 250, 0, 'Réservé', 9, 9),
+(100000010,'2025-01-01', 250, 0, 'Disponible',10,10);
+
+-- ========================
+-- 14) LIVRAISON (10)
+-- ========================
+INSERT INTO Livraison (livraison_ID, livraison_date, livraison_lieu, livraison_checks_docs, commande_ID) VALUES
+(1, '2025-03-15', 'Centre Porsche Paris', 'Oui', 1),
+(2, '2025-04-12', 'Centre Porsche Lyon', 'Oui', 2),
+(3, '2025-04-22', 'Centre Porsche Bordeaux', 'Non', 3),
+(4, '2025-05-03', 'Centre Porsche Paris', 'Oui', 4),
+(5, '2025-05-19', 'Centre Porsche Lyon', 'Non', 5),
+(6, '2025-06-02', 'Centre Porsche Marseille', 'Oui', 6),
+(7, '2025-06-16', 'Centre Porsche Rennes', 'Oui', 7),
+(8, '2025-07-04', 'Centre Porsche Paris', 'Oui', 8),
+(9, '2025-07-23', 'Centre Porsche Bordeaux', 'Oui', 9),
+(10,'2025-08-06', 'Centre Porsche Lille', 'Non', 10);
 
 -- ========================
 -- 15) PASSER (10)
--- (association Client-Commande)
 -- ========================
 INSERT INTO Passer (client_ID, commande_ID) VALUES
 (1,1),(2,2),(3,3),(4,4),(5,5),
